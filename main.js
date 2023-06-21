8d2235d30c8518d10eccea42a30ed1bed0939bc7
Abdul Moiz <221028@students.au.edu.pk>
2023-06-21 22:27:28 +0500

    Update main.js

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ options.forEach(element => {
             computer.classList.remove("shakeComputer");
             player.classList.remove("shakePlayer");
 
-            player.src = `./${element.innerHTML}Player.png`;
+            player.src = `/${element.innerHTML}Player.png`;
 
             const choice = ["ROCK", "PAPER", "SCISSORS"];
             let array = Math.floor(Math.random()*3);
             let computerChoice=choice[array];
-            computer.src =`./${computerChoice}Computer.png`;
+            computer.src =`/${computerChoice}Computer.png`;
 
             let cScore=parseInt(computerScore.innerHTML);
             let pScore=parseInt(playerScore.innerHTML);
